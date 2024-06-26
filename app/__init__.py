2d3e7601d7586247ece7b29ed029b7c46ac3c47b
warren yun <wyun2006@gmail.com>
2024-06-25 21:35:28 -0400

    about page

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Warren Yun", url=os.getenv("URL"))
+    return render_template('index.html', title="Warren Yun", url=os.getenv("URL"), about=os.getenv("ABOUT"))
+
+@app.route('/about')
+def about():
+    return render_template("more.html", title="Warren Yun", url=os.getenv("ABOUT"))
