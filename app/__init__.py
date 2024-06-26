ff2c7f86e22190d145084428cea56f8c21eac9f3
warren yun <wyun2006@gmail.com>
2024-06-25 22:02:29 -0400

    very cool dynamic navbar with jinja templates

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+routes = []
+for page in os.listdir(os.getcwd() + "/app/templates"):
+    if page != "index.html":
+        routes.append(page.removesuffix(".html"))
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Warren Yun", url=os.getenv("URL"), about=os.getenv("ABOUT"))
+    return render_template('index.html', title="Warren Yun", url=os.getenv("URL"), routes=routes)
 
 @app.route('/about')
 def about():
-    return render_template("more.html", title="Warren Yun", url=os.getenv("ABOUT"))
+    return render_template("about.html")
