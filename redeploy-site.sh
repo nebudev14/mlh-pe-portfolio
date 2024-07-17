760f02e12a22dd2f584379390328b25893e071f0
warren yun <wyun2006@gmail.com>
2024-07-17 03:51:58 -0400

    update redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,4 +4,5 @@ cd ~/mlh-pe-portfolio
 git fetch && git reset origin/main --hard
 python -m pip install -r "requirements.txt"
 source ./venv/bin/activate
-tmux new-session -d 'flask run --host=0.0.0.0'
+systemctl daemon-reload
+systemctl restart myportfolio
